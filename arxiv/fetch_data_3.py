087e6129eee60848a93739fa97cc8c78a76529f8
Six <qiu963258741@msn.com>
2017-06-02 10:20:19 -0400

    Update fetch_data_3.py
    update try catch and save every response as a separate file

--- arxiv/fetch_data_3.py
+++ arxiv/fetch_data_3.py
@@ -9,7 +9,7 @@ def get_raw_data():
 	print("request: %s" % (query))
 	request = urllib.request.Request(query)
 	response = urllib2.request.urlopen(request).read()
-	rawfile = open('papers.xml','w')
+	rawfile = open('papers_0.xml','w')
 	rawfile.write(response)
 	rawfile.close()
 	
@@ -17,36 +17,39 @@ def get_raw_data():
 	print("takes: %f s" % (end-start))
 	get_resume(response)
 
-def get_resume(response):
 	pos_start = response.rfind('<resumptionToken')
 	pos_end = response.rfind('</resumptionToken')
 	if pos_end > 0 and pos_end > pos_start:
 		pos = response.rfind('>', pos_start, pos_end)
 		resume_token = response[pos+1:pos_end]
 		print("request_resume: %s" % (resume_token))
+		get_resume(resume_token)
 
-		time.sleep(retry_time)
-
-		rawfile = open('papers.xml','a')
-		try:
-			start = time.clock()
-			query = "http://export.arxiv.org/oai2?verb=ListRecords&resumptionToken=%s" % (resume_token)
-			request = urllib.request.Request(query)
-			response = urllib.request.urlopen(request).read()
-		except Exception as err:
-			print(err)
-			print("retry resume_token: %s" % (resume_token))
-			start = time.clock()
-			time.sleep(60)
-			query = "http://export.arxiv.org/oai2?verb=ListRecords&resumptionToken=%s" % (resume_token)
-			request = urllib.request.Request(query)
-			response = urllib.request.urlopen(request).read()
-			
+def get_resume(token):
+	time.sleep(retry_time)
+	start = time.clock()
+	rawfile = open('papers_%s.xml'%(token),'w')
+	try:
+		query = "http://export.arxiv.org/oai2?verb=ListRecords&resumptionToken=%s" % (token)
+		request = urllib.request.Request(query)
+		response = urllib.request.urlopen(request).read()
 		rawfile.write(response)
 		rawfile.close()
 		end = time.clock()
 		print("takes: %f s" % (end-start))
-		get_resume(response)
+
+		pos_start = response.rfind('<resumptionToken')
+		pos_end = response.rfind('</resumptionToken')
+		if pos_end > 0 and pos_end > pos_start:
+			pos = response.rfind('>', pos_start, pos_end)
+			resume_token = response[pos+1:pos_end]
+			print("request_resume: %s" % (resume_token))
+			get_resume(resume_token)
+	except Exception as err:
+		print(err)
+		print("retry resume_token: %s" % (token))
+		time.sleep(30)
+		get_resume(token)
 
 if __name__ == '__main__':
 	get_raw_data()
